42178f48436e7fb98ac733e5525df564aaec06bd
jonathan <jonathanhinestroza87@gmail.com>
2022-06-12 16:32:30 -0500

    DNA Service finished

--- src/main/java/com/soulrebel/ml/service/DnaService.java
+++ src/main/java/com/soulrebel/ml/service/DnaService.java
@@ -1,20 +1,59 @@
 package com.soulrebel.ml.service;
 
+import com.google.gson.Gson;
 import com.soulrebel.ml.entity.Dna;
+import com.soulrebel.ml.model.StatisticsResponse;
+import com.soulrebel.ml.repository.DnaRepository;
 import com.soulrebel.ml.repository.GenericRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.text.DecimalFormat;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DnaService implements GenericRepository<Dna> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DnaService.class);
+
+    private final DnaRepository repository;
 
     @Override
     public void save(Dna entity) {
+        LOGGER.info("DNA is creating... Saved");
+        repository.save(entity);
 
     }
 
     @Override
-    public Optional<String> findAll() {
-        return Optional.empty();
+    public String findAll() {
+
+        LOGGER.info("DNA is creating... Saved");
+
+        StatisticsResponse statisticsResponse = new StatisticsResponse();
+        statisticsResponse.setCountMutantDna(repository.counterMutants());
+        statisticsResponse.setCountHumanDna(repository.counterMutants());
+
+        return getResponse(statisticsResponse);
+    }
+
+    private String getResponse(StatisticsResponse statisticsResponse) {
+        Double ratio;
+        String response;
+        if (statisticsResponse.getCountMutantDna() > 0 && statisticsResponse.getCountHumanDna() > 0) {
+            ratio = statisticsResponse.getCountMutantDna().doubleValue() / statisticsResponse.getCountHumanDna();
+            String ratioString = new DecimalFormat("#.##").format(ratio);
+            statisticsResponse.setRatio(Double.valueOf(ratioString));
+        } else {
+            statisticsResponse.setRatio(0.0);
+        }
+        response = new Gson().toJson(statisticsResponse);
+
+        return response;
     }
 }
