4f4d19f3ea2dc6ae14db13c4abcbc68c47726b2c
jonathan <jonathanhinestroza87@gmail.com>
2022-06-12 15:37:02 -0500

    DNA Service Created

--- src/main/java/com/soulrebel/ml/service/DnaService.java
+++ src/main/java/com/soulrebel/ml/service/DnaService.java
@@ -1,4 +1,20 @@
 package com.soulrebel.ml.service;
 
-public class DnaService {
+import com.soulrebel.ml.entity.Dna;
+import com.soulrebel.ml.repository.GenericRepository;
+
+import java.util.Optional;
+
+public class DnaService implements GenericRepository<Dna> {
+
+
+    @Override
+    public void save(Dna entity) {
+
+    }
+
+    @Override
+    public Optional<String> findAll() {
+        return Optional.empty();
+    }
 }
