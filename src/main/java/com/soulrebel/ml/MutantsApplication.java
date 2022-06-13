8ccdd2af1fe014b035975eca847175be0de7a16e
jonathan <jonathanhinestroza87@gmail.com>
2022-06-13 14:33:06 -0500

    Docker config

---
+++ src/main/java/com/soulrebel/ml/MutantsApplication.java
@@ -0,0 +1,22 @@
+package com.soulrebel.ml;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@SpringBootApplication
+@EnableSwagger2WebMvc
+@Slf4j
+public class MutantsApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MutantsApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting...");
+        SpringApplication.run(MutantsApplication.class, args);
+    }
+
+}
